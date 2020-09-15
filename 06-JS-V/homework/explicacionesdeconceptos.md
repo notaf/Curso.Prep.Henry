16f3a72acaf6700a6d15b15ea15cbc719545be90
Lucas Chiofalo <lucaschiofalo@gmail.com>
2020-09-14 23:28:53 -0300

    06-JS-V homework done

---
+++ 06-JS-V/homework/explicacionesdeconceptos.md
@@ -0,0 +1,5 @@
+1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+* `prototype`: Todos los objetos tienen una referencia a otro objeto llamada proto. 
+
+* _Constructors_ (de Clases): la function constructor es un prototipo, las clases son plantillas para la creacion de un modelo predefinido y se utilizan para representar entidades o conceptos.
--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,14 +9,37 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
 
-}
+function Usuario(opciones) {
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email; 
+    this.password = opciones.password;
+  }
+
+  Usuario.prototype.saludar = function() {
+    return "Hola, mi nombre es " + this.nombre;
+  };
+    return Usuario;
+  }
+
+
+  
+    
+    
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
 
+Constructor.prototype.saludar = function() {
+  return "Hello World!";
+};
 }
+
+
+
 
 function agregarStringInvertida() {
   // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
@@ -24,8 +47,23 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+ // Tu código:
 
+
+ String.prototype.reverse = function() {
+
+   var stringInvertida = "";
+
+  for (var i = this.length-1; i>=0; i--) { 
+   stringInvertida = stringInvertida + this[i];
+  }
+
+  return stringInvertida;
+
+ };
 }
+  
+  
 
 // No modificar nada debajo de esta línea
 // --------------------------------
